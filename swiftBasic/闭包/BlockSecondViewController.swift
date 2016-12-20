f4851f246447937f08952c46223a2bd945c424ca
Jiles <632036801@qq.com>
2016-12-20 23:23:42 +0800

    删除不必要的Log

--- swiftBasic/闭包/BlockSecondViewController.swift
+++ swiftBasic/闭包/BlockSecondViewController.swift
@@ -17,15 +17,12 @@ class BlockSecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.backClosure == nil ? print("yes") : print("no")
     }
     ///swift3.0默认闭包不可逃逸,所以需要声明逃逸闭包,闭包setter方法
     func setBackClosure(tempClosure: @escaping InputClosureType) {
         self.backClosure = tempClosure
-        self.backClosure == nil ? print("yes") : print("no")
     }
     @IBAction func backButtonPressed(_ sender: UIButton) {
-        self.backClosure == nil ? print("yes") : print("no")
         if self.backClosure != nil {
             let tempString : String? = self.textField.text
             if tempString != nil {
