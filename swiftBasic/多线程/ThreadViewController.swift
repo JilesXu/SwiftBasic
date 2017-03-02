f60039db014abea7cb1e80359fffb97a4fad5587
Jiles <632036801@qq.com>
2017-03-02 23:30:02 +0800

    更新GCD串并行队列，同异步执行

--- swiftBasic/多线程/ThreadViewController.swift
+++ swiftBasic/多线程/ThreadViewController.swift
@@ -22,16 +22,52 @@ class ThreadViewController: UIViewController {
 //        Thread.detachNewThreadSelector(#selector(run), toTarget: self, with: nil)
 //        print(Thread.current)
         /**********GCD**********/
-        //第一点：主队列
-        let queue = DispatchQueue.main
-        //第二点：全局队列
-        var queueGlobal = DispatchQueue.global(qos: .userInitiated)
-        // 如果不指定qos那就是默认default  global(qos: .default)
-        queueGlobal = DispatchQueue.global()
-        //第二点：自己创建的队列
-        let queue1 = DispatchQueue.init(label: "queue1", qos: .default, attributes:.concurrent)
-        print(queue)
-
+//        //第一点：主队列
+//        let queue = DispatchQueue.main
+//        //第二点：全局队列
+//        var queueGlobal = DispatchQueue.global(qos: .userInitiated)
+//        // 如果不指定qos那就是默认default  global(qos: .default)
+//        queueGlobal = DispatchQueue.global()
+//        //第二点：自己创建的队列
+//        let queue1 = DispatchQueue.init(label: "queue1", qos: .default, attributes:.concurrent)//并行队列
+//        //第三点：同步任务 会阻塞当前线程
+//        let queue2 = DispatchQueue.init(label: "queue2")
+//        queue2.sync {
+//            print("Hello World")
+//        }
+//        //第四点：异步任务 不会阻塞当前线程
+//        let queue3 = DispatchQueue(label: "queue3")
+//        queue3.async {
+//            print("Hello World")
+//        }
+//        print("Hello")
+        
+        /**********为了更好的理解同步和异步，和各种队列的使用，下面看两个示例**********/
+        /*****示例一*****/
+//        print("之前**********/\(Thread.current)")
+//        DispatchQueue.main.sync {
+//            print("sync**********/\(Thread.current)")
+//        }
+//        print("之后**********/\(Thread.current)")
+        
+        /*****示例二*****/
+//        let queue4 = DispatchQueue.init(label: "queue4")
+//        print("之前**********/\(Thread.current)")
+//        queue4.async {
+//            print("sync之前**********/\(Thread.current)")
+//            queue4.sync {
+//                print("sync**********/\(Thread.current)")
+//            }
+//            print("sync之后**********/\(Thread.current)")
+//        }
+//        print("之后**********/\(Thread.current)")
+        //第五点：延时执行
+        let delay = DispatchTime.now() + 5
+        
+        DispatchQueue.main.asyncAfter(deadline: delay) {
+            print(Thread.current)
+            print("hello world")
+        }
     }
     public func run() {
         print(Thread.current)
