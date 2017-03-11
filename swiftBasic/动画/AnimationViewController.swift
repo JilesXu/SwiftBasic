205ab241994eacea166adfb10e0ef5a09d997292
Jiles <632036801@qq.com>
2017-03-12 00:30:45 +0800

    新增动画

--- swiftBasic/动画/AnimationViewController.swift
+++ swiftBasic/动画/AnimationViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class AnimationViewController: UIViewController {
 
-    var userTF: UITextField!
-    var pswTF: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(userTF)
+        view.addSubview(pswTF)
         
+        var accountCenter = userTF.center
+        accountCenter.x += 200
+        
+        UIView.animate(withDuration: 1, delay: 0.5, options:.curveEaseInOut, animations: {
+            self.userTF.center = accountCenter
+        }) { (true) in
+            self.userTF.placeholder = "到位"
+        }
     }
     
-    //MARK - Setter And Getter
-    
+    //MARK: -Setter And Getter
+    lazy var userTF: UITextField = {
+        var userTF: UITextField = UITextField.init(frame: CGRect.init(x: -100, y: 200, width: 200, height: 50))
+        userTF.backgroundColor = UIColor.red
+        return userTF
+    }()
+    lazy var pswTF: UITextField = {
+        var pswTF: UITextField = UITextField.init(frame: CGRect.init(x: 100, y: 100, width: 200, height: 50))
+        pswTF.backgroundColor = UIColor.blue
+        return pswTF
+    }()
 }
