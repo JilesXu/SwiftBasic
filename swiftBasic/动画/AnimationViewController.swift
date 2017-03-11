89e4d8ca5e6fac58a6385db755062aac2351c0ea
Jiles <632036801@qq.com>
2017-03-11 23:48:57 +0800

    新增swift动画

---
+++ swiftBasic/动画/AnimationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AnimationViewController.swift
+//  swiftBasic
+//
+//  Created by 徐沈俊杰 on 2017/3/11.
+//  Copyright © 2017年 tinypace. All rights reserved.
+//
+
+import UIKit
+
+class AnimationViewController: UIViewController {
+
+    var userTF: UITextField!
+    var pswTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK - Setter And Getter
+    
+}
