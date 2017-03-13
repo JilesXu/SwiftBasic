55cd5dbb165b9151ad374679047c08937deae228
Jiles <632036801@qq.com>
2017-03-13 23:53:39 +0800

    增加动画

--- swiftBasic/动画/AnimationViewController.swift
+++ swiftBasic/动画/AnimationViewController.swift
@@ -16,25 +16,48 @@ class AnimationViewController: UIViewController {
         view.addSubview(userTF)
         view.addSubview(pswTF)
         
-        var accountCenter = userTF.center
-        accountCenter.x += 200
+        let viewBGCenter = self .view.center.x
+        var accountCenter = self.userTF.center
+        accountCenter.x = viewBGCenter
         
-        UIView.animate(withDuration: 1, delay: 0.5, options:.curveEaseInOut, animations: {
+        var passwordCenter = self.pswTF.center
+        passwordCenter.x = viewBGCenter
+        
+        UIView.animate(withDuration: 0.4, delay: 0.5, options:.curveEaseOut, animations: {
             self.userTF.center = accountCenter
         }) { (true) in
-            self.userTF.placeholder = "到位"
+            self.userTF.placeholder = "用户名"
         }
+        
+        UIView.animate(withDuration: 0.4, delay: 0.5, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            self.pswTF.center = passwordCenter
+        }) { (true) in
+            self.pswTF.placeholder = "密码"
+        }
+    }
+    
+    //MARK: -Events
+    func nextBtn(sender: UIButton) -> Void {
+        
     }
     
     //MARK: -Setter And Getter
     lazy var userTF: UITextField = {
-        var userTF: UITextField = UITextField.init(frame: CGRect.init(x: -100, y: 200, width: 200, height: 50))
+        var userTF: UITextField = UITextField.init(frame: CGRect.init(x: -300, y: 200, width: 200, height: 50))
         userTF.backgroundColor = UIColor.red
         return userTF
     }()
+    
     lazy var pswTF: UITextField = {
-        var pswTF: UITextField = UITextField.init(frame: CGRect.init(x: 100, y: 100, width: 200, height: 50))
+        var pswTF: UITextField = UITextField.init(frame: CGRect.init(x: -300, y: 100, width: 200, height: 50))
         pswTF.backgroundColor = UIColor.blue
         return pswTF
+    }()
+    
+    lazy var nextBtn: UIButton = {
+        var nextBtn: UIButton = UIButton.init(frame: CGRect.init(x: 0, y: 200, width: 100, height: 50))
+        nextBtn.setTitle("下一页", for: UIControlState.normal)
+        nextBtn.addTarget(self(), action: #selector(<#T##@objc method#>), for: UIControlEvents.touchUpInside)
+        return nextBtn
     }()
 }
---
+++ swiftBasic/动画/SecAnimationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecAnimationViewController.swift
+//  swiftBasic
+//
+//  Created by 徐沈俊杰 on 2017/3/13.
+//  Copyright © 2017年 tinypace. All rights reserved.
+//
+
+import UIKit
+
+class SecAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
